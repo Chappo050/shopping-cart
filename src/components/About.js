dcc3b45de062cc5d43f228dafc636c95b9c7c3d5
Chappo050 <matthewchaplin60@gmail.com>
2022-08-03 22:19:42 +1000

    About page done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
-
 import Nav from "./Nav";
 
 const About = () => {
   return (
-    
-    <div className="text-white font-bold text-center">
+    <div>
       <Nav />
-      ABOUT
+      <div className="text-white font-bold text-center m-10 text-2xl">
+        <div>
+          This shop is just for flasky fun and practice! 
+          <br/>
+          <br/>
+          Do not take this page seriously.
+          <br/> 
+          <br/>
+          Check out the code on my GitHub
+          <br/>
+          <br/>
+          <a className=" text-green-500 hover:text-green-800" href="https://github.com/Chappo050/shopping-cart">My GitHub</a>
+        </div>
+      </div>
     </div>
   );
 };
